7d6b0a15011b3af6e363f63928e5ad32dbbf8579
Benjamin <57809668+BunyaminUcar@users.noreply.github.com>
2023-04-12 18:32:08 +0300

    update_table

--- pages/basic_form.py
+++ pages/basic_form.py
@@ -7,58 +7,61 @@ from pages.pageBase import PageBase
 
 @pytest.mark.usefixtures("setup")
 class Form_Page(PageBase):
-    
-    
-    FORM_PAGE_TİTLE=(By.XPATH,"/html/body/div/h1")    
-    EMAİL_AREA=(By.XPATH,"//input[@type='text' and @name='username']")
-    PASSWORD_AREA=(By.XPATH,"//input[@type='password']")
-    TEXTAREA=(By.XPATH,"//textarea[@name='comments']")
-    SUBMIT_BUTTON=(By.XPATH,"//input[@type='submit']")
-    VALUE_USERNAME=(By.XPATH,"//*[@id='_valueusername']")
-    VALUE_PASSWORD=(By.XPATH,"//*[@id='_valuepassword']")   
-    VALUE_TEXTAREA=(By.XPATH,"//*[@id='_valuecomments']")
-    CHECK_BOX1=(By.XPATH,"//input[@type='checkbox' and @value='cb1']")
-    CHECK_BOX2=(By.XPATH,"//input[@type='checkbox' and @value='cb2']")
-    CHECK_BOX3=(By.XPATH,"//input[@type='checkbox' and @value='cb3']")
-    RADIO_BUTTON1=(By.XPATH,"//input[@type='radio' and @value='rd1']")
-    RADIO_BUTTON2=(By.XPATH,"//input[@type='radio' and @value='rd2']")
-    RADIO_BUTTON3=(By.XPATH,"//input[@type='radio' and @value='rd3']")
+
+    FORM_PAGE_TİTLE = (By.XPATH, "/html/body/div/h1")
+    EMAİL_AREA = (By.XPATH, "//input[@type='text' and @name='username']")
+    PASSWORD_AREA = (By.XPATH, "//input[@type='password']")
+    TEXTAREA = (By.XPATH, "//textarea[@name='comments']")
+    SUBMIT_BUTTON = (By.XPATH, "//input[@type='submit']")
+    VALUE_USERNAME = (By.XPATH, "//*[@id='_valueusername']")
+    VALUE_PASSWORD = (By.XPATH, "//*[@id='_valuepassword']")
+    VALUE_TEXTAREA = (By.XPATH, "//*[@id='_valuecomments']")
+    CHECK_BOX1 = (By.XPATH, "//input[@type='checkbox' and @value='cb1']")
+    CHECK_BOX2 = (By.XPATH, "//input[@type='checkbox' and @value='cb2']")
+    CHECK_BOX3 = (By.XPATH, "//input[@type='checkbox' and @value='cb3']")
+    RADIO_BUTTON1 = (By.XPATH, "//input[@type='radio' and @value='rd1']")
+    RADIO_BUTTON2 = (By.XPATH, "//input[@type='radio' and @value='rd2']")
+    RADIO_BUTTON3 = (By.XPATH, "//input[@type='radio' and @value='rd3']")
+
     def __init__(self, driver):
         super().__init__(driver)
         self.driver = driver
-        
+
     def get_text_from_the_element(self):
-        title=self.driver.find_element(*Form_Page.FORM_PAGE_TİTLE).text
+        title = self.driver.find_element(*Form_Page.FORM_PAGE_TİTLE).text
         return title
-    
-    def send_email_and_password(self,email,password):
+
+    def send_email_and_password(self, email, password):
         self.driver.find_element(*Form_Page.EMAİL_AREA).send_keys(email)
         self.driver.find_element(*Form_Page.PASSWORD_AREA).send_keys(password)
-        
+
     def click_submit_button(self):
         self.driver.find_element(*Form_Page.SUBMIT_BUTTON).click()
-        
-    def get_email_value(self): 
+
+    def get_email_value(self):
         return self.driver.find_element(*Form_Page.VALUE_USERNAME).text
+
     def get_password_value(self):
         return self.driver.find_element(*Form_Page.VALUE_PASSWORD).text
 
     def radio_select(self):
-        return self.driver.find_element(*Form_Page.RADIO_BUTTON1),self.driver.find_element(*Form_Page.RADIO_BUTTON2),self.driver.find_element(*Form_Page.RADIO_BUTTON3)
-    
-    def email_is_visible(self): 
+        return self.driver.find_element(*Form_Page.RADIO_BUTTON1), self.driver.find_element(*Form_Page.RADIO_BUTTON2), self.driver.find_element(*Form_Page.RADIO_BUTTON3)
+
+    def email_is_visible(self):
         wait = WebDriverWait(self.driver, 1)
         try:
-            wait.until(EC.visibility_of_element_located((Form_Page.VALUE_USERNAME)))
+            wait.until(EC.visibility_of_element_located(
+                (Form_Page.VALUE_USERNAME)))
             result = True
         except:
             result = False
         return result
-    
-    def password_is_visible(self): 
+
+    def password_is_visible(self):
         wait = WebDriverWait(self.driver, 1)
         try:
-            wait.until(EC.visibility_of_element_located((Form_Page.VALUE_PASSWORD)))
+            wait.until(EC.visibility_of_element_located(
+                (Form_Page.VALUE_PASSWORD)))
             result = True
         except:
             result = False
@@ -67,25 +70,26 @@ class Form_Page(PageBase):
     def get_textarea_value(self):
         return self.driver.find_element(*Form_Page.VALUE_TEXTAREA).text
 
-    def send_textarea_value(self,key):
+    def send_textarea_value(self, key):
         self.driver.find_element(*Form_Page.TEXTAREA).clear()
         self.driver.find_element(*Form_Page.TEXTAREA).send_keys(key)
+
     def checkbox_select(self):
-        return self.driver.find_element(*Form_Page.CHECK_BOX1),self.driver.find_element(*Form_Page.CHECK_BOX2),self.driver.find_element(*Form_Page.CHECK_BOX3)
-          
-    def if_checkbox_is_not_selected_then_click(self,checkbox):
-        if checkbox.is_selected()==False:
+        return self.driver.find_element(*Form_Page.CHECK_BOX1), self.driver.find_element(*Form_Page.CHECK_BOX2), self.driver.find_element(*Form_Page.CHECK_BOX3)
+
+    def if_checkbox_is_not_selected_then_click(self, checkbox):
+        if checkbox.is_selected() == False:
             checkbox.click()
-        
-    def if_radio_button_is_not_selected_then_click(self,radio):
-        
-        if radio.is_selected()==False:
+
+    def if_radio_button_is_not_selected_then_click(self, radio):
+
+        if radio.is_selected() == False:
             radio.click()
+
     def get_m_s_value(self):
-        return self.driver.find_element(By.XPATH,"//option[@value='ms1']"),self.driver.find_element(By.XPATH,"//option[@value='ms2']"),self.driver.find_element(By.XPATH,"//option[@value='ms3']"),self.driver.find_element(By.XPATH,"//option[@value='ms4']")
-    
-            
-    def if_multiple_select_is_not_selected_then_click(self,ms):
-        
-        if ms.is_selected()==False:
+        return self.driver.find_element(By.XPATH, "//option[@value='ms1']"), self.driver.find_element(By.XPATH, "//option[@value='ms2']"), self.driver.find_element(By.XPATH, "//option[@value='ms3']"), self.driver.find_element(By.XPATH, "//option[@value='ms4']")
+
+    def if_multiple_select_is_not_selected_then_click(self, ms):
+
+        if ms.is_selected() == False:
             ms.click()
--- test_dynamic_table.py
+++ test_dynamic_table.py
@@ -1,17 +1,40 @@
+import json
 import pytest
 from selenium.webdriver.common.by import By
 from pages.basic_form import Form_Page
 import time
 
+
 @pytest.mark.usefixtures("setup")
 class Test_Table():
-    
 
     @pytest.fixture(autouse=True)
     def class_setup(self):
-        self.driver.get("https://testpages.herokuapp.com/styled/tag/dynamic-table.html")
-        self.forms=Form_Page(self.driver)
-        
-    def test_table_name(self):
-        
-        assert self.driver.find_element(By.XPATH,"//*[@id='dynamictable']/caption").text=="Dynamic Table"
+        self.driver.get(
+            "https://testpages.herokuapp.com/styled/tag/dynamic-table.html")
+        self.forms = Form_Page(self.driver)
+
+    def test_table_form(self):
+
+        assert self.driver.find_element(
+            By.XPATH, "//h1").text == "Dynamic HTML TABLE Tag"
+
+    def test_dynamic_table(self):
+        self.driver.find_element(
+            By.XPATH, "/html/body/div/div[3]/details/summary").click()
+        time.sleep(1)
+        payload = [{"name": "Sam", "age": 20},
+                   {"name": "Millie", "age": 32},
+                   {"name": "Joe", "age": 36},
+                   {"name": "Ana", "age": 22}]
+        payload_str = json.dumps(payload)
+        self.driver.find_element(By.XPATH, "//*[@id='jsondata']").clear()
+        self.driver.find_element(By.XPATH, "//*[@id='jsondata']").send_keys(payload_str)
+        self.driver.find_element(By.XPATH, "//*[@id='caption']").clear()
+        self.driver.find_element(By.XPATH, "//*[@id='caption']").send_keys("Dinamik Tablo")
+        self.driver.find_element(By.XPATH,"//*[@id='tableid']").clear()
+        self.driver.find_element(By.XPATH,"//*[@id='tableid']").send_keys("dynamictableid")
+        self.driver.find_element(By.XPATH,"//*[@id='refreshtable']").click()
+
+        time.sleep(5)
+
