3db0e4b10bc990d97bba4c46eae73f73b5d83c8a
Benjamin <57809668+BunyaminUcar@users.noreply.github.com>
2023-04-18 16:47:26 +0300

    test_alerts

--- pages/alerts.py
+++ pages/alerts.py
@@ -7,8 +7,28 @@ from pages.pageBase import PageBase
 
 @pytest.mark.usefixtures("setup")
 class Alert_Page(PageBase):
-    
+    SİMPLE_ALERT = (By.XPATH, "//*[@id='alertexamples']")
+    CONFİRM_BOX_ALERT = (By.XPATH, "//*[@id='confirmexample']")
+    CONFİRM_BOX_VARİABLE = (By.XPATH, "//*[@id='confirmreturn']")
+
     def __init__(self, driver):
         super().__init__(driver)
         self.driver = driver
-        
+
+    def click_simple_alert_buton(self):
+        self.driver.find_element(*Alert_Page.SİMPLE_ALERT).click()
+
+    def switch_to_alert(self):
+        return self.driver.switch_to.alert
+
+    def alert_accept(self):
+        self.driver.switch_to.alert.accept()
+
+    def alert_dismiss(self):
+        self.driver.switch_to.alert.dismiss()
+
+    def click_confirm_box_buton(self):
+        self.driver.find_element(*Alert_Page.CONFİRM_BOX_ALERT).click()
+
+    def confirm_box_variable(self):
+        return self.driver.find_element(*Alert_Page.CONFİRM_BOX_VARİABLE).text
--- test_alerts.py
+++ test_alerts.py
@@ -1,51 +1,49 @@
 import time
 import pytest
 from selenium.webdriver.common.by import By
+from pages.alerts import Alert_Page
 from pages.basic_form import Form_Page
 
 
 @pytest.mark.usefixtures("setup")
-class Test_Alerts():
-
+class Test_Alerts:
     @pytest.fixture(autouse=True)
     def class_setup(self):
-        self.driver.get(
-            "https://testpages.herokuapp.com/styled/alerts/alert-test.html")
-        self.forms = Form_Page(self.driver)
+        self.driver.get("https://testpages.herokuapp.com/styled/alerts/alert-test.html")
+        self.alerts = Alert_Page(self.driver)
 
+    @pytest.mark.skip
     def test_simple_alert_box(self):
-
-        self.driver.find_element(
-            By.XPATH, "//*[@id='alertexamples']").click()
-        alert = self.driver.switch_to.alert
+        self.alerts.click_simple_alert_buton()
+        alert = self.alerts.switch_to_alert()
         alert_text = alert.text
-        self.driver.switch_to.alert.accept()
+        self.alerts.alert_accept()
         assert alert_text == "I am an alert box!"
 
+    @pytest.mark.skip
     def test_show_confirm_box_then_accept(self):
-
-        self.driver.find_element(By.XPATH, "//*[@id='confirmexample']").click()
-        alert = self.driver.switch_to.alert
+        self.alerts.click_confirm_box_buton()
+        alert = self.alerts.switch_to_alert()
         alert_text = alert.text
-        self.driver.switch_to.alert.accept()
-        assert self.driver.find_element(
-            By.XPATH, "//*[@id='confirmreturn']").text == "true"
+        self.alerts.alert_accept()
+        assert self.alerts.confirm_box_variable() == "true"
         assert alert_text == "I am a confirm alert"
 
     def test_show_confirm_box_then_dismiss(self):
-
-        self.driver.find_element(By.XPATH, "//*[@id='confirmexample']").click()
-        alert = self.driver.switch_to.alert
+        self.alerts.click_confirm_box_buton()
+        alert = self.alerts.switch_to_alert()
         alert_text = alert.text
-        self.driver.switch_to.alert.dismiss()
-        assert self.driver.find_element(
-            By.XPATH, "//*[@id='confirmreturn']").text == "false"
+        self.alerts.alert_dismiss()
+        assert self.alerts.confirm_box_variable() == "false"
         assert alert_text == "I am a confirm alert"
 
+    @pytest.mark.skip
     def test_show_prompt_box_then_send_keys_and_accept(self):
         self.driver.find_element(By.XPATH, "//*[@id='promptexample']").click()
         time.sleep(5)
         self.driver.switch_to.alert.send_keys("test prompt box")
-        assert self.driver.find_element(
-            By.XPATH, "//*[@id='promptreturn']").text == "test prompt box"
+        assert (
+            self.driver.find_element(By.XPATH, "//*[@id='promptreturn']").text
+            == "test prompt box"
+        )
         time.sleep(3)
