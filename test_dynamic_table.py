7a09ed7984bd4e4989c8c9721c3cc8584c0d7f33
Benjamin <57809668+BunyaminUcar@users.noreply.github.com>
2023-04-20 20:42:45 +0300

    Update test_dynamic_table.py

--- test_dynamic_table.py
+++ test_dynamic_table.py
@@ -1,4 +1,3 @@
-
 import pytest
 from selenium.webdriver.common.by import By
 from pages.basic_form import Form_Page
@@ -8,16 +7,13 @@ from pages.table import Dynamic_Table
 
 
 @pytest.mark.usefixtures("setup")
-class Test_Table():
-
+class Test_Table:
     @pytest.fixture(autouse=True)
     def class_setup(self):
-        self.driver.get(
-            "https://testpages.herokuapp.com/styled/tag/dynamic-table.html")
+        self.driver.get("https://testpages.herokuapp.com/styled/tag/dynamic-table.html")
         self.dynamicTable = Dynamic_Table(self.driver)
 
     def test_table_form(self):
-
         assert self.dynamicTable.get_table_TAG() == "Dynamic HTML TABLE Tag"
 
     def test_dynamic_table(self):
