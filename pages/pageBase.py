a3b7189f44afc13c9e01954f535baa988c2c9c9d
Benjamin <57809668+BunyaminUcar@users.noreply.github.com>
2023-04-26 13:22:36 +0300

    Update pageBase.py

--- pages/pageBase.py
+++ pages/pageBase.py
@@ -1,8 +1,7 @@
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC 
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class PageBase:
-    
     def __init__(self, driver):
         self.driver = driver
