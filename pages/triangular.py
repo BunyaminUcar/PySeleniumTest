13d5b19c049192bb6d9d3decd27201ddd107da64
Benjamin <57809668+BunyaminUcar@users.noreply.github.com>
2023-04-26 13:22:30 +0300

    Update triangular.py

--- pages/triangular.py
+++ pages/triangular.py
@@ -6,23 +6,27 @@ from selenium.webdriver.support.ui import Select
 
 @pytest.mark.usefixtures("setup")
 class Triangular(PageBase):
+    FIRST_CORNER = (By.XPATH, "//*[@id='side1']")
+    SECOND_CORNER = (By.XPATH, "//*[@id='side2']")
+    THIRD_CORNER = (By.XPATH, "//*[@id='side3']")
+    CALCULATE_BUTTON = (By.XPATH, "//*[@id='identify-triangle-action']")
+    RESULT_TRIANGULAR = (By.XPATH, "//*[@id='triangle-type']")
+
     def __init__(self, driver):
         super().__init__(driver)
         self.driver = driver
 
     def enter_the_first_corner(self, side):
-        self.driver.find_element(By.XPATH, "//*[@id='side1']").send_keys(side)
+        self.driver.find_element(*Triangular.FIRST_CORNER).send_keys(side)
 
     def enter_the_second_corner(self, side):
-        self.driver.find_element(By.XPATH, "//*[@id='side2']").send_keys(side)
+        self.driver.find_element(*Triangular.SECOND_CORNER).send_keys(side)
 
     def enter_the_third_corner(self, side):
-        self.driver.find_element(By.XPATH, "//*[@id='side3']").send_keys(side)
+        self.driver.find_element(*Triangular.THIRD_CORNER).send_keys(side)
 
     def calculate_tringular(self):
-        self.driver.find_element(
-            By.XPATH, "//*[@id='identify-triangle-action']"
-        ).click()
+        self.driver.find_element(*Triangular.CALCULATE_BUTTON).click()
 
     def get_result(self):
-        return self.driver.find_element(By.XPATH, "//*[@id='triangle-type']").text
+        return self.driver.find_element(*Triangular.RESULT_TRIANGULAR).text
