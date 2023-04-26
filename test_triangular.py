23fd95524fd52c8d471d44da40f658c930344bff
Benjamin <57809668+BunyaminUcar@users.noreply.github.com>
2023-04-26 13:17:18 +0300

    test_try_to_draw_a_not_drawable_triangular

--- test_triangular.py
+++ test_triangular.py
@@ -2,24 +2,30 @@ import time
 import pytest
 from selenium.webdriver.common.by import By
 
+from pages.triangular import Triangular
+
 
 @pytest.mark.usefixtures("setup")
-class Test_Calculator:
+class Test_Triangular:
     @pytest.fixture(autouse=True)
     def class_setup(self):
         self.driver.get(
             "https://testpages.herokuapp.com/styled/apps/triangle/triangle001.html"
         )
+        self.triangular = Triangular(self.driver)
 
-    def test_3_4_5_triangular(self):
-        self.driver.find_element(By.XPATH, "//*[@id='side1']").send_keys("3")
-        self.driver.find_element(By.XPATH, "//*[@id='side2']").send_keys("4")
-        self.driver.find_element(By.XPATH, "//*[@id='side3']").send_keys("5")
-        self.driver.find_element(
-            By.XPATH, "//*[@id='identify-triangle-action']"
-        ).click()
-        assert (
-            self.driver.find_element(By.XPATH, "//*[@id='triangle-type']").text
-            == "Scalene"
-        )
-        time.sleep(3)
+    def test_draw_a_drawable_triangle(self):
+        self.triangular.enter_the_first_corner("3")
+        self.triangular.enter_the_second_corner("4")
+        self.triangular.enter_the_third_corner("5")
+        self.triangular.calculate_tringular()
+        result = self.triangular.get_result()
+        assert result == "Scalene"
+
+    def test_try_to_draw_a_not_drawable_triangular(self):
+        self.triangular.enter_the_first_corner("38")
+        self.triangular.enter_the_second_corner("440")
+        self.triangular.enter_the_third_corner("1")
+        self.triangular.calculate_tringular()
+        result = self.triangular.get_result()
+        assert result == "Error: Not a Triangle"
